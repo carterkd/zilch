af71cdbcfeb86d6083f34f7456b0635703c3f695
Carter Davis <carterkentdavis@gmail.com>
2026-06-17 19:59:59 -0400

    Add Firebase project config

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,12 @@
 // Fill these public Firebase web app values after creating the Firebase project.
 // The app keeps online rooms disabled until the placeholders are replaced.
 window.ZILCH_FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCmtET4Q1XWJwSZoJtro1s4gVBjLARLQEc",
+  authDomain: "zilch-e6ec7.firebaseapp.com",
+  databaseURL: "https://zilch-e6ec7-default-rtdb.firebaseio.com",
+  projectId: "zilch-e6ec7",
+  storageBucket: "zilch-e6ec7.firebasestorage.app",
+  messagingSenderId: "962313550223",
+  appId: "1:962313550223:web:0fcb2a7cdfabcf26f973fa",
+  measurementId: "G-07GWHBQ5FV"
 };
